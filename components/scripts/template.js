bb8db80085fe9c0e87b8ef9dd7da910bd6ae4785
ruelasb <bruest@hotmail.com>
2016-12-06 13:35:08 -0800

    First Commit

---
+++ components/scripts/template.js
@@ -0,0 +1,11 @@
+
+$(function() {
+  var Mustache = require('mustache');
+
+  $.getJSON('js/data.json', function(data) {
+    var template = $('#speakerstpl').html();//get the data inside the script(get template layout)
+    var html = Mustache.to_html(template, data);//merge the data in script with the json data as html(make template layout)
+    $('#speakers').html(html);//place merged data inside the #speakers article as html (place html template layout)
+  }); //getJSON
+  
+}); //function
