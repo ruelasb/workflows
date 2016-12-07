51e0bcf7ee4a36f95de9b9b1e37e75f5036586d5
ruelasb <bruest@hotmail.com>
2016-12-07 10:57:30 -0800

    added compass process

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var	gutil = require('gulp-util');//used to log stuff to terminal
 var coffee = require('gulp-coffee');//used to turn coffee files into .js files
-var browserify = require('gulp-browserify');
+var browserify = require('gulp-browserify');//gets all require variables and pulls all libraries
+var compass = require('gulp-compass');//gets all @import files and concat to css
 var concat = require('gulp-concat');//concatinates all files in order of object/array
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -11,6 +12,8 @@ var jsSources = [
 	'components/scripts/tagline.js',
 	'components/scripts/template.js'
 ];
+
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function(){
 	gulp.src(coffeeSources)
@@ -22,6 +25,17 @@ gulp.task('coffee', function(){
 gulp.task('js', function(){
 	gulp.src(jsSources)
 	.pipe(concat('script.js'))
-	.pipe(browserify())
+	.pipe(browserify()) //looks through all thre requies and gets code from libraries
 	.pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function(){
+	gulp.src(sassSources)
+	.pipe(compass({
+      sass: 'components/sass',
+      images: 'builds/development/images',
+      style: 'expanded'
+    	}))
+		.on('error', gutil.log)
+	.pipe(gulp.dest('builds/development/css'))
 });
